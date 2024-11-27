9887978be079829661f65b590c928ebfd41480d1
Menfhis <alfredoxd00@hotmail.com>
2024-11-27 18:29:13 -0400

    Custom Provider and Pagination

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { CommonModule } from 'src/common/common.module';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
 import { PokemonModule } from 'src/pokemon/pokemon.module';
@@ -6,6 +7,6 @@ import { PokemonModule } from 'src/pokemon/pokemon.module';
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
-  imports: [PokemonModule]
+  imports: [PokemonModule, CommonModule]
 })
 export class SeedModule { }
